7616fa1f39a086c5dec5216f914193d5e6d3e540
苟富军 <750108747@qq.com>
2021-12-25 16:12:22 +0800

    first commit

---
+++ src/main/java/io/longtu/cloud_music/controller/DefaultController.java
@@ -0,0 +1,15 @@
+package io.longtu.cloud_music.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class DefaultController {
+
+    @GetMapping
+    public String sayHello() {
+        return "欢迎网抑云音乐";
+    }
+}
